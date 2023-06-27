eb2d7e27b65c0b685b879d0b768afabbe7c81c59
Shank8466 <137475517+Shank8466@users.noreply.github.com>
2023-06-27 21:55:14 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,60 @@
+# Arithmetic operators
+a = 15
+b = 10
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+print("floor division: ",a//b)
+print("exponent: ",a**b)
+print("module: ",a%b)
+
+# Assignment operators
+A = 20
+B = 30
+A +=5
+print("Value of A: ",A)
+B-=5
+print("Value of B: ",B)
+A*=5
+print("Value of A is: ",A)
+B/=3
+print("Value of B: ",B)
+A%=5
+print("Modulus of A is: ",A)
+B//=2
+print("Value of B is: ",B)
+
+# Comparison Operators
+print(">:",a>b)
+print("<",a<b)
+print("==",a==b)
+print("!=",a!=b)
+print("==",a==b)
+print("!=",a!=b)
+
+# Logical operators
+x = True
+y = False
+print("and",x and y)
+print("or",x or y)
+print("not",not y)
+
+# Bitwise Operator
+print("Bitwise &",a&b)
+print("Bitwise |",a|b)
+print("Bitwise <<",a<<b)
+print("Bitwise >>",a>>b)
+
+# Identity Operators
+a=10
+b=5
+print("is",a is b) # Compares the location 
+print("is not",a is not b)
+
+# Membership Operators
+S = "Shank"
+print("S" in S)
+print("i" not in S)
+
+
